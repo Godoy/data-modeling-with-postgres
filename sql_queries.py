37d07cbc37cf244326cc38d936815bc3cf413f3b
Adriano Godoy <adriano.godoy@xpi.com.br>
2021-05-30 16:33:42 -0300

    Finishes ETL python script based on notebook

--- sql_queries.py
+++ sql_queries.py
@@ -8,57 +8,60 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
+
 songplay_table_create = ("""
-    CREATE TABLE songplays (timestamp TIMESTAMP, user_id INT, level TEXT, song_id TEXT, artist_id TEXT, session_id INT, location TEXT, user_agent TEXT)
+    CREATE TABLE songplays (songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP, user_id INT, level TEXT, song_id TEXT, artist_id TEXT, session_id INT, location TEXT, user_agent TEXT)
 """)
 
 user_table_create = ("""
-    CREATE TABLE users (first_name TEXT, gender CHAR, last_name TEXT, level TEXT, user_id INT)
+    CREATE TABLE users (user_id INT PRIMARY KEY, first_name TEXT, gender CHAR, last_name TEXT, level TEXT)
 """)
 
 song_table_create = ("""
-    CREATE TABLE songs 
-        (id TEXT PRIMARY KEY, title TEXT, duration DECIMAL, year INT, artist_id TEXT NULL            
-        );
+    CREATE TABLE songs (song_id TEXT PRIMARY KEY, title TEXT, duration DECIMAL, year INT, artist_id TEXT NULL);
 """)
 # CONSTRAINT fk_artist FOREIGN KEY(artist_id) REFERENCES artists(id)
 
 artist_table_create = ("""
-    CREATE TABLE artists (id TEXT PRIMARY KEY, name TEXT, latitude TEXT, longitude TEXT, location TEXT);
+    CREATE TABLE artists (artist_id TEXT PRIMARY KEY, name TEXT, latitude TEXT, longitude TEXT, location TEXT);
 """)
 
 time_table_create = ("""
-    CREATE TABLE time (timestamp TIMESTAMP, hour INT, day INT, week_of_year INT, month INT, year INT, weekday INT)
+    CREATE TABLE time (start_time TIMESTAMP PRIMARY KEY, hour INT, day INT, week INT, month INT, year INT, weekday INT)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-    INSERT INTO songplays (timestamp, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
-    INSERT INTO users (first_name, gender, last_name, level, user_id) VALUES (%s, %s, %s, %s, %s);
+    INSERT INTO users (user_id, first_name, gender, last_name, level) VALUES (%s, %s, %s, %s, %s) 
+    ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
-    INSERT INTO songs (id, title, duration, year, artist_id) VALUES (%s, %s, %s, %s, %s);
+    INSERT INTO songs (song_id, title, duration, year, artist_id) VALUES (%s, %s, %s, %s, %s) 
+    ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
-    INSERT INTO artists (id, name, latitude, longitude, location) VALUES (%s, %s, %s, %s, %s);
+    INSERT INTO artists (artist_id, name, latitude, longitude, location) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 time_table_insert = ("""
-    INSERT INTO time (timestamp, hour, day, week_of_year, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) 
+    ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
-    SELECT s.id songid, s.artist_id artistid
+    SELECT s.song_id songid, s.artist_id artistid
     FROM songs s
-    LEFT JOIN artists a ON (s.artist_id = a.id)
+    LEFT JOIN artists a ON (s.artist_id = a.artist_id)
     WHERE s.title = %s AND a.name = %s AND s.duration = %s 
 """)
 
